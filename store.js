2230f4e20820b8778c4438561fc12a98cbefc8e4
Brent <bantalan35@gmail.com>
2023-08-08 14:17:12 +1000

    4.6 workshop steps

--- store.js
+++ store.js
@@ -1,5 +1,29 @@
 // We can export multiple functions as an object:
 
+function Employee(id, firstName, lastName, gender, age, position) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.age = age;
+    this.position = position;
+}
+
+function Sale(staffId, item, price, date) {
+    this.staffId = staffId;
+    this.item = item;
+    this.price = price;
+    this.date = date;
+}
+
+function showEmployeeDetails() {
+    console.log(`Name: ${employee.firstName} ${employee.lastName}\nStaff ID: ${employee.id}\nAge and Gender: ${employee.age}, ${employee.gender}\nPosition: ${employee.position}`);
+}
+
+function showSaleDetails () {
+    console.log(`Date of sale: ${sale.date}\nItem sold: ${sale.item}\nItem price: ${sale.price}\n Staff ID of seller: ${sale.staffId}`);
+}
+
 module.exports = {
     Employee: Employee,
     Sale: Sale,
